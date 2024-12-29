9863fbb734c2556c4c2d8bcbd3e7bb988d8a0438
gfrobles <gfrobles@gmail.com>
2024-12-29 10:47:14 -0800

    added htmlnode class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 from textnode import TextType, TextNode
+from htmlnode import HTMLNode
 
 def main():
     print("hello world")
     textNode = TextNode("This is a text node", TextType.BOLD,"https://www.boot.dev")
     print(textNode)
+
+    props_test= {
+    "href": "https://www.google.com", 
+    "target": "_blank",
+    }
+    child = HTMLNode("h1", "Title")
+    htmlNode = HTMLNode("p", "Testing my code",[child], props_test)
+    print(htmlNode)
 
 if __name__ == "__main__":
     main()
